ba9489174a8c69465cd07ba95a1e7ca442bee49f
Mitchell Troy <mtroy2@nd.edu>
2015-04-22 21:24:39 -0400

    Began Bowser Class and Attacks

---
+++ Bowser.cpp
@@ -0,0 +1,163 @@
+/*
+ * Bowser.cpp
+ *
+ *  Created on: Apr 19, 2015
+ *      Author: naiello
+ */
+
+#include "Bowser.h"
+#include "ObjectLayer.h"
+#include "Hammer.h"
+Bowser::Bowser(Uint32 id) : Enemy(id)
+{
+
+}
+
+void Bowser::init(ContentManager* content)
+{
+ 	srand (time(NULL));
+	setName("Bowser");
+	setBoundingBox({0, 0, BOWSER_WIDTH, BOWSER_HEIGHT});
+	setMaxHealth(BOWSER_HEALTH);
+	setHealth(BOWSER_HEALTH);
+	setContactDamage(50);		// Damage taken by player for walking into the Bowser
+
+	sprite = content->getAnimatedTexture("sprites/M-Bowser2.png", 0, 5, 50, 44, 0, 1, 7);
+	sprite.addAnimation("shellSpin", 340,230,50,44,0,4);
+	sprite.addAnimation("enraged",84 ,226 , 50, 44, 0, 3);
+	sprite.addAnimation("bowserJump",50,62,50,44,0,4);
+	velocity.x = 0;
+}
+
+void Bowser::onWalkIntoWall(WorldObject* wall, const SDL_Rect& overlap)
+{
+	jump();
+}
+
+void Bowser::draw(SDL_Renderer* renderer)
+{
+	/*SDL_Rect bbox = getCamera()->transform(getBoundingBox());
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderDrawRect(renderer, &bbox);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);*/
+	Vector2d tpos = getCamera()->transform(position);
+	sprite.draw(renderer, tpos.x - 12, tpos.y);
+	Enemy::draw(renderer);
+}
+void Bowser::spitFlames(){
+
+	Hammer* ham;
+	Vector2d fpos;
+	if (hammerCooldown > 0.){}
+	else
+	{
+		sprite.setAnimation("ranged");
+		hammerCooldown = HAMMER_COOLDOWN;
+		ham = new Hammer(WorldObject::getUniqueID());
+		fpos = {(facingLeft) ? position.x : position.x + BOWSER_WIDTH, position.y - BOWSER_HEIGHT};
+		ham->setPosition(fpos);
+		if (facingLeft)
+			ham->reverseDirection();
+		getParentLayer()->addObject(ham);
+	}
+
+
+}
+void Bowser::squish(){
+	die();
+}
+void Bowser::update(Uint32 time)
+{
+	double secs = time / 1000.;
+	hammerCooldown -= secs;
+	animTimer -= secs;
+	if (animTimer >= 1.5)
+	{
+		switch (state)
+		{
+		case STANDING:
+			velocity.x = 0;
+			break;
+		case MVG_RIGHT:
+			velocity.x = BOWSER_WALKSPD;
+			break;
+		case MVG_LEFT:
+			velocity.x = -BOWSER_WALKSPD;
+		}
+	}
+	else if (animTimer < 1.5 && animTimer >= 0 )
+	{
+		switch (state)
+		{
+		case STANDING:
+			break;
+		case MVG_RIGHT:
+			stop();
+			break;
+		case MVG_LEFT:
+			stop();
+			break;
+		}
+
+	}
+	else{
+		int randWalk = rand() % 100;
+		
+		if ( randWalk > 49 )
+		{
+			animTimer = ANIMATION_TIMER;
+			walkLeft();
+		} 
+		else if (randWalk <= 49)
+		{
+			animTimer = ANIMATION_TIMER;
+			walkRight();	
+		}
+	}
+	throwHammer();
+	if (framesSinceTouchedGround++ > 2)
+		inAir = true;
+
+	if (inAir)
+		velocity.y += GRAVITY * secs;
+	else
+		velocity.y = 0.;
+
+	WorldObject::update(time);
+
+}
+void Bowser::walkLeft()
+{
+	if(state != MVG_LEFT)
+	{
+		state = MVG_LEFT;
+		facingLeft = true;
+		sprite.setFlipH(true);
+
+	}
+}
+void Bowser::walkRight()
+{
+	if(state != MVG_RIGHT)
+	{
+		state = MVG_RIGHT;
+		facingLeft = false;
+		sprite.setFlipH(false);
+
+	}
+}
+void Bowser::stop()
+{
+	velocity.x = 0;
+}
+void Bowser::jump()
+{
+	if (!inAir && canJump)
+	{
+		inAir = true;
+		canJump = false;
+		position.y -= 5;		
+		velocity.y = -300;
+	}
+		
+} 
---
+++ Bowser.h
@@ -0,0 +1,63 @@
+/*
+ * Bowser.h
+ *
+ *  Created on: Apr 22, 2015
+ *      Author: mtroy2
+ */
+
+
+#ifndef BOWSER_H_
+#define BOWSER_H_
+
+#include "Enemy.h"
+#include <cstdlib>
+#include <ctime>
+#define BOWSER_WALKSPEED 15
+#define BOWSER_HEALTH 300
+#define BOWSER_WIDTH 35
+#define BOWSER_HEIGHT 41
+#define FIREBALL_COOLDOWN 5.0
+#define JUMP_COOLDOWN 3.0
+#define ANIMATION_TIMER 3.00
+#define BOWSER_WALKSPD 20
+#define BOWSER_JUMPSPD 100
+#define SHELLSPIN_COOLDOWN 10
+enum BowserState
+{
+	BOW_STANDING,
+	BOW_MVG_RIGHT,
+	BOW_MVG_LEFT
+};
+class Bowser : public Enemy 
+{
+public:
+	Bowser(Uint32);
+	
+	void init(ContentManager*);
+	void draw(SDL_Renderer*);
+	void onWalkIntoWall(WorldObject*, const SDL_Rect&);
+	void squish();
+	void update(Uint32);
+	void walkLeft();
+	void walkRight();
+	void stop();
+	
+private:
+	//flags
+	bool enraged = false // when low on hp
+	bool canJump = true;
+	int state = STANDING;
+	bool facingLeft = true;
+	AnimatedTexture sprite;
+	// attacks
+	void spitFlames();
+	void shellSpin();
+	void jump();	
+	// cool downs
+	double fireBallCoolDown = 0;
+	double jumpCoolDown = 0;
+	double animTimer = ANIMATION_TIMER;
+	double shellSpinCoolDown = 0;
+
+};
+#endif
